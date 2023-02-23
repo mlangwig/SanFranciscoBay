975024381cccd4da5f51d42b54b3d10f83630fb8
Marguerite Langwig <marguerite.langwig@gmail.com>
2023-02-23 11:18:02 -0600

    clean up reads barplot

--- Geochem/Geochem.R
+++ Geochem/Geochem.R
@@ -19,6 +19,10 @@ geochem <- select(geochem, c(SiteFull, Site, Month, Temp, Sal,
 #melt to long format
 geochem_long <- melt(geochem, id = c("SiteFull", "Site", "Month"))
 
+#change Jul to July
+geochem_long$Month <- gsub("Jul", "July", geochem_long$Month)
+geochem_long$Month <- factor(geochem_long$Month, levels = c("July", "Oct", "Jan", "May"))
+
 ############################ Set order of sites for plotting #############################
 #set order of x axis 
 geochem_long$Site <- factor(geochem_long$Site, levels=c("4_1", "8_1", "13", "21", "24"))
@@ -28,9 +32,7 @@ geochem_long$SiteFull <- factor(geochem_long$SiteFull, levels=c("4_1_July", "4_1
                                                                           "13_July", "13_Oct", "13_Jan", "13_May",
                                                                           "21_July", "21_Oct", "21_Jan", "21_May",
                                                                           "24_July", "24_Oct", "24_Jan", "24_May"))
-#change Jul to July
-geochem_long$Month <- gsub("Jul", "July", geochem_long$Month)
-geochem_long$Month <- factor(geochem_long$Month, levels = c("July", "Oct", "Jan", "May"))
+
 ####################################### Plotting Supplemental ##########################################
 
 library(grid)
@@ -68,4 +70,60 @@ plot
 
 # , width = 15, height = 7,
 ggsave("Output/Geochem_Supplemental.png", plot, width = 15, dpi = 500)
+
+################## Making input for main figure geochem ##########################################
+
+geochem_sal <- geochem_long %>% filter(grepl(c("Sal"), variable))
+
+############################ Set order of sites for plotting #############################
+#set order of x axis 
+geochem_sal$Site <- factor(geochem_sal$Site, levels=c("4_1", "8_1", "13", "21", "24"))
+#set order of x axis 
+geochem_sal$SiteFull <- factor(geochem_sal$SiteFull, levels=c("4_1_July", "4_1_Oct", "4_1_Jan", "4_1_May",
+                                                                "8_1_July", "8_1_Oct", "8_1_Jan", "8_1_May",
+                                                                "13_July", "13_Oct", "13_Jan", "13_May",
+                                                                "21_July", "21_Oct", "21_Jan", "21_May",
+                                                                "24_July", "24_Oct", "24_Jan", "24_May"))
+
+
+############### plotting #############
+
+text_2011 <- textGrob("2011", gp=gpar(fontsize=15, fontface="bold"))
+text_2012 <- textGrob("2012", gp=gpar(fontsize=15, fontface="bold"))
+
+dev.off()
+p2 <- geochem_sal %>%
+  ggplot(aes(x = Month, y = as.numeric(value), group = Site)) + #group to make this behave
+  geom_line(aes(color = Site), size = 2) +
+  #scale_fill_manual(values = col_vector) +
+  labs(x = "Month", y = "Salinity (PSU)") +
+  #guides(fill=guide_legend(override.aes = list(size=3))) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5),
+    text = element_text(size = 20),
+    legend.key.size = unit(1, 'cm'),
+    #legend.background = element_rect(color = "white"),
+    #legend.box.background = element_rect(fill = "transparent"),
+    panel.background = element_rect(fill = "transparent"),
+    #panel.grid.major = element_blank(),
+    #panel.grid.minor = element_blank(),
+    plot.background = element_rect(fill = "transparent", color = NA)) +
+  #panel.border = element_blank()) + #turn this off to get the outline back)
+  #scale_y_continuous(expand = c(0, 0), breaks = c(7,9,11,13,15,17,19,21)) + #turn this on to make it look aligned with ticks
+  scale_color_manual(values = c("#4F508C", "#B56478", "#CF9A28", "#28827A", "#3F78C1"),
+                     name = "",
+                     labels = c("4.1", "8.1", "13", "21", "24")) +
+  ggtitle("") +
+  annotation_custom(text_2011, xmin=2.1,xmax=1,ymin=-43) +
+  annotation_custom(text_2012, xmin=6,xmax=1,ymin=-43) +
+  coord_cartesian(ylim = c(0,31), xlim = c(1.5,3.5), clip="off") #messed with xlim to get less space between plot and margin
+#scale_y_continuous(breaks = seq(0, 45, by=5))
+#coord_flip()
+p2
+
+
+#save
+ggsave("Output/Salinity.png", p2, dpi = 500)
+
+
 
--- Reads_Barplot/ReadsBarPlot.R
+++ Reads_Barplot/ReadsBarPlot.R
@@ -1,5 +1,4 @@
 ############################ Reads Stacked Bar Plot ##########################################
-setwd("~/Google Drive/My Drive/SF_Bay/SanFranciscoBay/Reads_Barplot/")
 
 library(dplyr)
 library(tidyverse)
@@ -9,156 +8,75 @@ library(viridis)
 library(pals)
 library(RColorBrewer)
 
-data <- read.delim("input/Reads_inMAGs_totals.txt")
-
-#rename sites
-data <- data %>% separate(Site, c("Month", "Site"), 
-                                          sep= "_USGS_")
-data$Month<-gsub("SF_*", "", data$Month)
-data$Month<-gsub("12|11", "", data$Month)
-data$Month<-gsub("Jul", "July", data$Month)
-
-data$Site <- paste0(data$Site, "_", data$Month)
-
-#set order of x axis 
-data$Site <- factor(data$Site, levels=c("4_1_July", "4_1_Oct", "4_1_Jan", "4_1_May",
-                                                  "8_1_July", "8_1_Oct", "8_1_Jan", "8_1_May",
-                                                  "13_July", "13_Oct", "13_Jan", "13_May",
-                                                  "21_July", "21_Oct", "21_Jan", "21_May",
-                                                  "24_July", "24_Oct", "24_Jan", "24_May")) 
-  
-#melt the data
-data_melt<-data[, c("Site", "NumReads", "NumMAGReads")]
-data_melt <- melt(data_melt, id = "Site") 
-
-dev.off()
-#plot
-p<-ggplot(data_melt, aes(fill=variable, y=value, x=Site, label = value)) + 
-  geom_bar(position="stack", stat="identity") +
-  scale_fill_manual(values = c("#bababa", "#d6604d")) +
-  theme(axis.text.x = element_text(angle = 90, vjust = .5),
-        #panel.background = element_blank(),
-        axis.ticks = element_blank()) +
-  scale_y_continuous(expand = c(0, 0)) +
-  geom_text(size = 1.5, position = position_stack(vjust = 0.5))
-p
-
-p + annotate(geom="text", x="4_1_July", y=30000000, label="19.1%", size = 2,
-               color="black") +
-  annotate(geom="text", x="4_1_Oct", y=37000000, label="22.3%", size = 2,
-             color="black") +
-  annotate(geom="text", x="4_1_Jan", y=40000000, label="25.1%", size = 2,
-             color="black") +
-  annotate(geom="text", x="4_1_May", y=38000000, label="18.9%", size = 2,
-             color="black") +
-  annotate(geom="text", x="8_1_July", y=30000000, label="18%", size = 2,
-             color="black") +
-  annotate(geom="text", x="8_1_Oct", y=38000000, label="20.7%", size = 2,
-             color="black") +
-  annotate(geom="text", x="8_1_Jan", y=38000000, label="22.5%", size = 2,
-             color="black") +
-  annotate(geom="text", x="8_1_May", y=10000000, label="2.3%", size = 2,
-             color="black") +
-  annotate(geom="text", x="13_July", y=24000000, label="14.8%", size = 2,
-             color="black") +
-  annotate(geom="text", x="13_Oct", y=14000000, label="5.7%", size = 2,
-             color="black") +
-  annotate(geom="text", x="13_Jan", y=19000000, label="7.8%", size = 2,
-             color="black") +
-  annotate(geom="text", x="13_May", y=43000000, label="24.6%", size = 2,
-             color="black") +
-  annotate(geom="text", x="21_July", y=13000000, label="5.3%", size = 2,
-             color="black") +
-  annotate(geom="text", x="21_Oct", y=15000000, label="7%", size = 2,
-             color="black") +
-  annotate(geom="text", x="21_Jan", y=48000000, label="19.8%", size = 2,
-             color="black") +
-  annotate(geom="text", x="21_May", y=21000000, label="9.2%", size = 2,
-             color="black") +
-  annotate(geom="text", x="24_July", y=11000000, label="4.1%", size = 2,
-             color="black") +
-  annotate(geom="text", x="24_Oct", y=21000000, label="8.6%", size = 2,
-             color="black") +
-  annotate(geom="text", x="24_Jan", y=15000000, label="6%", size = 2,
-             color="black") +
-  annotate(geom="text", x="24_May", y=12000000, label="4.6%", size = 2,
-             color="black")
-
-#example input data
-# specie <- c(rep("sorgho" , 3) , rep("poacee" , 3) , rep("banana" , 3) , rep("triticum" , 3) )
-# condition <- rep(c("normal" , "stress" , "Nitrogen") , 4)
-# value <- abs(rnorm(12 , 0 , 15))
-# test <- data.frame(specie,condition,value)
-
 ############################ Coverm Input Data ##########################################
 
-data2 <- read.delim("input/SFBMAGS-vs-Reads-CoverM.tsv")
+data <- read.delim("input/SFBMAGS-vs-Reads-CoverM.tsv")
 
 #rename sites
-colnames(data2) = gsub("_filter.METAGENOME.fastq.gz.Relative.Abundance....", "", colnames(data2))
+colnames(data) = gsub("_filter.METAGENOME.fastq.gz.Relative.Abundance....", "", colnames(data))
 #colnames(data2) = gsub("_sed_USGS", "", colnames(data2))
 #colnames(data2) = gsub("SF_", "", colnames(data2))
 
 #transform from wide to long
-data2_long <- melt(data2, id = c("Genome")) 
+data_long <- melt(data, id = c("Genome")) 
 
 #rename variable column to Site
-data2_long<-rename(data2_long, "Site" = "variable")
+data_long<-rename(data_long, "Site" = "variable")
 
 #clean up Site names
-data2_long <- data2_long %>% separate(Site, c("Month", "Site"), 
+data_long <- data_long %>% separate(Site, c("Month", "Site"), 
                           sep= "_USGS_")
-data2_long$Month<-gsub("SF_*", "", data2_long$Month)
-data2_long$Month<-gsub("12|11", "", data2_long$Month)
-data2_long$Month<-gsub("Jul", "July", data2_long$Month)
-data2_long$Month<-gsub("_sed", "", data2_long$Month)
+data_long$Month<-gsub("SF_*", "", data_long$Month)
+data_long$Month<-gsub("12|11", "", data_long$Month)
+data_long$Month<-gsub("Jul", "July", data_long$Month)
+data_long$Month<-gsub("_sed", "", data_long$Month)
 
-data2_long$Site <- paste0(data2_long$Site, "_", data2_long$Month)
+data_long$Site <- paste0(data_long$Site, "_", data_long$Month)
 
 #set order of x axis 
-data2_long$Site <- factor(data2_long$Site, levels=c("4_1_July", "4_1_Oct", "4_1_Jan", "4_1_May",
+data_long$Site <- factor(data_long$Site, levels=c("4_1_July", "4_1_Oct", "4_1_Jan", "4_1_May",
                                         "8_1_July", "8_1_Oct", "8_1_Jan", "8_1_May",
                                         "13_July", "13_Oct", "13_Jan", "13_May",
                                         "21_July", "21_Oct", "21_Jan", "21_May",
                                         "24_July", "24_Oct", "24_Jan", "24_May")) 
 
 #subset only unmapped
-data2_long_plotting<-data2_long[data2_long$Genome=="unmapped",]
+data_long_plotting<-data_long[data_long$Genome=="unmapped",]
 
 #get rid of unmapped so can sum by site for percent mapped
-data2_long_mapped<-data2_long[!grepl("unmapped", data2_long$Genome),]
+data_long_mapped<-data_long[!grepl("unmapped", data_long$Genome),]
 
 #sum values for percent mapped
-data2_long_mapped <- data2_long_mapped %>%
+data_long_mapped <- data_long_mapped %>%
   group_by(Site) %>% 
   summarise(value=sum(value)) #summing the group
 
 #combine mapped and unmapped into 1 data frame
-data2_long_plotting<-rename(data2_long_plotting, "Unmapped Reads" = "value")
-data2_long_plotting<-select(data2_long_plotting, -Genome)
+data_long_plotting<-rename(data_long_plotting, "Unmapped Reads" = "value")
+data_long_plotting<-select(data_long_plotting, -Genome)
 
-data2_long_mapped<-rename(data2_long_mapped, "Mapped Reads" = "value")
+data_long_mapped<-rename(data_long_mapped, "Mapped Reads" = "value")
 
 ##vlookup
-data2_long_plotting <- data2_long_mapped %>%
+data_long_plotting <- data_long_mapped %>%
   dplyr::select("Site", "Mapped Reads") %>%
-  right_join(data2_long_plotting, by = c("Site" = "Site"))
+  right_join(data_long_plotting, by = c("Site" = "Site"))
 
 #melt the data
-data2_melt<-data2_long_plotting[, c("Site", "Mapped Reads", "Unmapped Reads")]
-data2_melt <- melt(data2_melt, id = "Site") 
+data_melt<-data_long_plotting[, c("Site", "Mapped Reads", "Unmapped Reads")]
+data_melt <- melt(data_melt, id = "Site") 
 
 #add site without month column as option for faceting
-data2_melt$Site2 = data2_melt$Site
-data2_melt <- data2_melt %>% separate(Site2, c("SiteOnly", "Month"), sep= "(?=_[A-Z])")
+data_melt$Site2 = data_melt$Site
+data_melt <- data_melt %>% separate(Site2, c("SiteOnly", "Month"), sep= "(?=_[A-Z])")
 
 #set order of x axis 
-data2_melt$SiteOnly <- factor(data2_melt$SiteOnly, levels=c("4_1", "8_1", "13", "21", "24")) 
+data_melt$SiteOnly <- factor(data_melt$SiteOnly, levels=c("4_1", "8_1", "13", "21", "24")) 
 
 #plot
 dev.off()
 #plot
-p<-ggplot(data2_melt, aes(fill=forcats::fct_rev(variable), y=value, x=Site, label = round(value, digits = 2))) + 
+p<-ggplot(data_melt, aes(fill=forcats::fct_rev(variable), y=value, x=Site, label = round(value, digits = 2))) + 
   geom_bar(position="stack", stat="identity") +
   scale_fill_manual(values = c("#9ecae1", "#3182bd")) +
   theme(axis.text.x = element_text(angle = 90, vjust = .5),
